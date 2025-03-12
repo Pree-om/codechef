542c24ed84ec60e4b31d4234f0e7d36519dd03fe
PREE OM <ompree29@gmail.com>
2025-03-12 23:30:21 +0530

    Add files via upload

---
+++ DoubleRent.java
@@ -0,0 +1,31 @@
+/* Double Rent
+Chefina decided to move into Chef's apartment.
+Chef was initially paying a rent of 
+X
+X rupees. Since Chefina is moving in, the owner decided to double the rent.
+
+Find the final rent Chef needs to pay.
+
+Input Format
+The input consists of a single integer 
+X
+X, denoting the rent Chef was initially paying.
+
+Output Format
+Output on a new line, the final rent Chef needs to pay. */
+
+//CODE:
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		System.out.println(X*2);
+
+	}
+}
